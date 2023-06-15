86e867557fd133d1c347c3088313abd6a595d1d1
Atom <dat101196@gmail.com>
2023-06-15 16:34:14 +0700

    Finish Lesson 4 -Import component -props -emit event -teleport component -event modifier -slot Add new project game demo

--- lesson_4/src/App.vue
+++ lesson_4/src/App.vue
@@ -1,27 +1,69 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <h1>Hello from Atom 2</h1>
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <!-- <HelloWorld ref="hello" message="Welcome to Your Vue.js App" /> -->
+  <!--  -->
+  <my-header ref="header" mes="Message pass to header from App" />
+  <h1>Hello from {{ myName }}</h1>
+  <button @click="onChangeName">Change name</button>
+  <!-- Teleport modal component to body -->
+  <teleport to="body">
+    <base-modal
+      v-if="isShowModal"
+      title="Confirmation"
+      :body="bodyModal"
+      @cancel="onToggleModal"
+    >
+      <template v-slot:header>
+        <h3>Slot header</h3>
+      </template>
+
+      <template v-slot:footer>
+        <button @click="onToggleModal">Close</button>
+      </template>
+
+      <label>Name</label>
+      <input type="text" />
+    </base-modal>
+  </teleport>
+  <button @click="onToggleModal">Toggle modal</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from "./components/HelloWorld.vue";
+import Header from "./components/MainHeader.vue";
+import ModalDialog from "./components/ModalDialog.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    // HelloWorld,
+    MyHeader: Header,
+    BaseModal: ModalDialog,
+  },
+  data() {
+    return {
+      myName: "Dang Anh Tam",
+      bodyModal: "Modal content bind from data of App.vue",
+      isShowModal: false,
+    };
+  },
+  methods: {
+    onChangeName() {
+      this.myName = "Dang Atom";
+      console.log(this.$refs.hello);
+      console.log(this.$refs.header);
+      console.log("headerName: ", this.$refs.header.headerName);
+      this.$refs.header.headerName = "Tam Header";
+      console.log("headerName change to: ", this.$refs.header.headerName);
+      this.$refs.hello.onConsoleTest();
+    },
+    onToggleModal() {
+      console.log("onToggleModal");
+      this.isShowModal = !this.isShowModal;
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ lesson_4/src/components/MainHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>This is {{ headerName }} component.</h1>
+  <p class="modal-header">Message:"{{ mes }}"</p>
+  <hr/>
+</template>
+
+<script>
+export default {
+  props: {
+    mes: String,
+  },
+  data() {
+    return {
+      headerName: "Atom Header",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
