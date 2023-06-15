b39efae8fe24c2848cfe87ddcb4ef526570e907e
Atom <dat101196@gmail.com>
2023-06-15 17:11:03 +0700

    Add screen component

---
+++ game-memories-example/src/components/CopyRightScreen.vue
@@ -0,0 +1,6 @@
+<template>
+  <p>
+    This game owned by RHP Team in Vue 3 course for beginners -
+    <a href="#">View here</a>
+  </p>
+</template>
---
+++ game-memories-example/src/components/MainScreen.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="screen">
+    <h1>POKE MEMORIES</h1>
+    <h3>Select mode to start game</h3>
+    <div class="action">
+      <button>
+        <span>4x4</span>
+        <span>easy</span>
+      </button>
+      <button>
+        <span>6x6</span>
+        <span>Normal</span>
+      </button>
+      <button>
+        <span>8x8</span>
+        <span>Hard</span>
+      </button>
+      <button>
+        <span>10x10</span>
+        <span>Insane</span>
+      </button>
+    </div>
+  </div>
+</template>
