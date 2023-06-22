ea45660a2661a21fb4e937d6bbf51810b4f14613
Atom <dat101196@gmail.com>
2023-06-22 17:09:43 +0700

    Learn vue : ref

--- lesson_5/src/pages/home-page.vue
+++ lesson_5/src/pages/home-page.vue
@@ -1,5 +1,24 @@
 <template>
   <div>
     <h1>Overview Page</h1>
+    <p>{{ firstName }}</p>
+    <button @click="onChangeSomething">Click me</button>
   </div>
 </template>
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    const firstName = ref("aaa");
+
+    function onChangeSomething() {
+      firstName.value = "Atom";
+    }
+
+    return {
+      firstName,
+      onChangeSomething,
+    };
+  },
+};
+</script>
