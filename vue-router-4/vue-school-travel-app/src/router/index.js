3d8244053df63b40ef530bfadfb3456c0d082845
Atom <dat101196@gmail.com>
2023-07-04 17:07:06 +0700

    Finish chapter 2: -Add go-back component -Add route props -Add children route

--- vue-router-4/vue-school-travel-app/src/router/index.js
+++ vue-router-4/vue-school-travel-app/src/router/index.js
@@ -10,15 +10,39 @@ const routes = [
     { path: '/', name: 'Home', component: Home },
     //Lazy loading routes
     //Sử dụng arrow function để import view => khi nào url trỏ để view nào mới load code của view tương ứng. Do ko phải import tất cả view từ đầu nên không load tất cả view khi load/reload trang
-    { path: '/destination/:id/:slug', name:'destination.show', component: () =>import('@/views/DestinationShow.vue') },
-    
+    {
+        path: '/destination/:id/:slug',
+        name: 'destination.show',
+        component: () => import('@/views/DestinationShow.vue'),
+        // Nếu ko cần phải xử lý param trong route thì ta chỉ cần khai báo props: true như bên dưới. 
+        // props: true,
+        //Trường hợp cần xử lý param trong route thì ta làm như bên dưới. Những param nào cần xử lý thì destructuring cụ thể ra còn lại dùng rest parameter.
+        props: route => ({ ...route.params, id: parseInt(route.params.id) }),
+        children: [
+            //Children route:
+            //Muốn hiển thị children route thì trong template của DestinationShow.vue (parent) phải có router-view. Khi đó thay vì nhấn vô router-link để chuyển vô view được định nghĩa bên dưới thì sẽ hiện trong router-view của parent
+            {
+                path: ':experienceSlug',
+                name: 'experience.show',
+                component: () => import('@/views/ExperienceShow.vue'),
+                props: route => ({ ...route.params, id: parseInt(route.params.id) })
+            }
+        ]
+    },
+    //Normal route
+    // {
+    //     path: '/destination/:id/:slug/:experienceSlug',
+    //     name: 'experience.show',
+    //     component: () => import('@/views/ExperienceShow.vue'),
+    //     props: route => ({ ...route.params, id: parseInt(route.params.id) })
+    // }
 
     //Normal loading routes
     //Phải import tất cả view từ đầu nên sẽ nặng và chậm hơn khi load/reload trang
     // { path: '/brazil', name: 'Brazil', component: Brazil },
     // { path: '/hawaii', name: 'Hawaii', component: Hawaii },
     // { path: '/panama', name: 'Panama', component: Panama },
-    
+
 ]
 
 const router = createRouter({
--- vue-router-4/vue-school-travel-app/src/views/DestinationShow.vue
+++ vue-router-4/vue-school-travel-app/src/views/DestinationShow.vue
@@ -1,17 +1,89 @@
 <template>
-   <section class="destination">
-    <h1>{{ destination.name }}</h1>
-    <div class="destination-details">
-        <img :src="`/images/${destination.image}`" :alt="destination.name">
-        <p>{{ destination.description }}</p>
+    <!-- Giao diện đối với trường hợp get data từ API
+    Cần phải v-if để check có data mới render UI -->
+    <!-- <div v-if="des">
+        <section class="destination">
+            <h1>{{ des.name }}</h1>
+            <div class="destination-details">
+                <img :src="`/images/${des.image}`" :alt="des.name">
+                <p>{{ des.description }}</p>
+            </div>
+        </section>
+        <section class="experiences">
+            <h2>Top Experiences in {{ des.name }}</h2>
+            <div class="cards">
+                <router-link v-for="exp in des.experiences" :key="exp.slug"
+                    :to="{ name: 'experience.show', params: { experienceSlug: exp.slug } }">
+                    <ExperienceCard :experience="exp" />
+                </router-link>
+            </div>
+            <router-view></router-view>
+        </section>
     </div>
-   </section>
+    <p v-else>Loading...</p> -->
+    <section class="destination">
+        <div class="destination-header">
+            <GoBack />
+            <h1 class="destination-header__title">{{ des.name }}</h1>
+        </div>
+        <div class="destination-details">
+            <img :src="`/images/${des.image}`" :alt="des.name">
+            <p>{{ des.description }}</p>
+        </div>
+    </section>
+    <section class="experiences">
+        <h2>Top Experiences in {{ des.name }}</h2>
+        <div class="cards">
+            <router-link v-for="exp in des.experiences" :key="exp.slug"
+                :to="{ name: 'experience.show', params: { experienceSlug: exp.slug } }">
+                <ExperienceCard :experience="exp" />
+            </router-link>
+        </div>
+        <router-view></router-view>
+    </section>
 </template>
 <script setup>
 import { useRoute } from "vue-router";
-import { computed } from 'vue';
+import { ref, watchEffect, computed } from 'vue';
 import sourceData from '@/data.json';
+import ExperienceCard from "@/components/ExperienceCard.vue";
+import GoBack from "@/components/GoBack.vue";
 const route = useRoute();
+
+//define props
+const props = defineProps({
+    //id, slug, experienceSlug là property of route.params. Thay vì dùng route.params.id, route.params.slug, route.params.experienceSlug thì ta khai báo props để hứng và trong file router/index.js ta thêm props: true trong route dẫn tới trang này.
+    //Lưu ý: ta cần tạo props hứng tất cả param truyền trong route để tránh warning từ vue
+    id: {
+        type: Number,
+        required: true,
+    },
+    slug: String,
+    experienceSlug: String,
+});
+//Get data from data.json file
+//Dùng props.id thay thế route.params.id
 // const destinationId = computed(() => parseInt(route.params.id));
-const destination = computed(async() => await fetch(`https://travel-dummy-api.netlify.app/${slug}.json`));
+const des = computed(() => {
+    console.log('props.id: ', props.id);
+    return sourceData.destinations.find(des => des.id == props.id)
+});
+
+console.log('props slug: ', props.slug);
+console.log('props experienceSlug: ', props.experienceSlug);
+
+//Code get data from API
+//Cần tạo ref để hứng data get từ API và dùng watchEffect để theo dõi khi route param (hoặc route props) thay đổi thì thực hiện get data lại 
+// let des = ref(null);
+// const getDestination = async () => {
+//     const response = await fetch(`https://travel-dummy-api.netlify.app/${props.slug}.json`);
+//     des.value = await response.json();
+//     console.log(des);
+// }
+
+// watchEffect(() => {
+//     getDestination();
+// });
+
+
 </script>
