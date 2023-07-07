16b30a32e51896e80f2ee900df18f6253dc1624f
Atom <dat101196@gmail.com>
2023-07-07 17:23:07 +0700

    Add first HomeView UI

--- vue-typescript/toEat/src/views/HomeView.vue
+++ vue-typescript/toEat/src/views/HomeView.vue
@@ -1,9 +1,24 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue'
+const restaurantList = ref<string[]>([]);
+const newRestaurantName = ref('');
+function addRestaurant() {
+  restaurantList.value.push(newRestaurantName.value);
+}
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <!-- Crate a form that allows users to add a restaurant to list. -->
+    <form @submit.prevent="addRestaurant">
+      <label for="restaurant-name">Restaurant Name</label>
+      <input type="text" id="restaurant-name" v-model="newRestaurantName">
+      <button type="submit">Add Restaurant</button>
+    </form>
+    <ul>
+      <li v-for="restaurant in restaurantList" :key="restaurant">
+        {{ restaurant }}
+      </li>
+    </ul>
   </main>
 </template>
