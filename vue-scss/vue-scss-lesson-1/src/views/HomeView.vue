2bf8614676fffe79ce720c000c1e37b31e4aa7a8
Atom <dat101196@gmail.com>
2023-07-10 21:08:46 +0700

    First test scss

---
+++ vue-scss/vue-scss-lesson-1/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+  <div class="hello">Hello <span class="child-hello">World</span></div>
+</template>
+
+<style lang="scss">
+@import '@/assets/style.scss';
+</style>
