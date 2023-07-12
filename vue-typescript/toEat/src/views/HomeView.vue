73f747ca56210bf76332136d77995f634f49a85e
Atom <dat101196@gmail.com>
2023-07-12 22:09:19 +0700

    Add learn emit, props in typescript

--- vue-typescript/toEat/src/views/HomeView.vue
+++ vue-typescript/toEat/src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import DishesPage from './DishesPage.vue'
 //import type and status list external
-import type { RecommendStatus, IDish } from '../types'
-import { RecommendStatusList } from '../constants'
+import type { IRestaurant } from '@/models/restaurant'
+import { RecommendStatusList } from '@/constants'
 //Pinia
 import { storeToRefs } from 'pinia'
-import { useCounterStore } from '../stores/counter'
+import { useCounterStore } from '@/stores/counter'
+import type { IDish } from '@/models/dish'
 
 //Chỉ có thể destructuring hàm vì counter được bọc bởi reactive nếu destructuring thì sẽ bị mất khả năng reactive như props trong setup
 const counter = useCounterStore();
@@ -46,54 +48,67 @@ const { increment } = counter;
 // type RecommendStatus = typeof RecommendStatusList[number];
 //
 
-interface IRestaurant {
-  name?: string,
-  status?: RecommendStatus,
-  dishes?: IDish[]
-}
+
 
 const restaurantList = ref<IRestaurant[]>([]);
 const newRestaurant = ref<IRestaurant>({
+  id: restaurantList.value.length + 1,
   //Enum
   // status: RecommendStatus.WantToTry
   //Type
   status: 'Want to Try'
 });
 
-
-
 function addRestaurant() {
-  let restaurant: IRestaurant = {
-    name: newRestaurant.value.name,
-    status: newRestaurant.value.status,
-    dishes: []
+  const restaurant: IRestaurant = {
+    ...newRestaurant.value
   }
-  restaurantList.value.push(restaurant);
+  console.log('[addRestaurant] restaurant: ', restaurant)
+  restaurantList.value.push(restaurant)
 }
+
+const addDish = (dish: IDish) => {
+  newRestaurant.value.dishes?.push(dish)
+    console.log('[addDish] newRestaurant: ', newRestaurant.value)
+    console.log('[addDish] newRestaurant: ', newRestaurant.value.dishes)
+}
+
+// const dishesList = ref<IDish[]>(props.restaurant.dishes ?? []);
 </script>
 
 <template>
-  <main>
-    <!-- Create a form that allows users to add a restaurant to list. -->
-    <pre>
+  <div class="main-view">
+    <div class="restaurant-form">
+      <h2>Restaurant</h2>
+      <h3>Info</h3>
+      <!-- Create a form that allows users to add a restaurant to list. -->
+      <pre>
       {{ newRestaurant }}
     </pre>
-    <form @submit.prevent="addRestaurant">
-      <div>
-        <label for="restaurant-name">Restaurant Name</label>
-        <input type="text" id="restaurant-name" v-model="newRestaurant.name">
-      </div>
-      <div>
-        <label for="restaurant-status">Restaurant Status</label>
-        <select name="restaurant-status" id="restaurant-status" v-model="newRestaurant.status">
-          <option v-for="status in RecommendStatusList" :value="status" :key="status">{{ status }}</option>
-        </select>
-      </div>
-      <button type="submit">Add Restaurant</button>
-    </form>
+      <h3>Edit form</h3>
+      <form @submit.prevent="addRestaurant">
+        <div>
+          <label for="restaurant-name">Restaurant Name</label>
+          <input type="text" id="restaurant-name" v-model="newRestaurant.name">
+        </div>
+        <div>
+          <label for="restaurant-status">Restaurant Status</label>
+          <select name="restaurant-status" id="restaurant-status" v-model="newRestaurant.status">
+            <option v-for="status in RecommendStatusList" :value="status" :key="status">{{ status }}</option>
+          </select>
+        </div>
+        <DishesPage :restaurant="newRestaurant" @addDishToRestaurant="addDish" />
+        <button type="submit">Add Restaurant</button>
+      </form>
+
+    </div>
+
     <ul>
       <li v-for="(restaurant, index) in restaurantList" :key="index">
         {{ restaurant.name }} - {{ restaurant.status }}
+        <pre>
+          {{ restaurant.dishes?.length }}
+        </pre>
       </li>
     </ul>
     <!-- Cách dùng Pinia ko destructuring -->
@@ -102,8 +117,26 @@ function addRestaurant() {
     <button @click="counter.increment">Increment</button> -->
 
     <!-- Cách dùng Pinia destructuring -->
-    <p>Count: {{ count }}</p>
+    <!-- <p>Count: {{ count }}</p>
     <p>Double: {{ doubleCount }}</p>
-    <button @click="increment">Increment</button>
-  </main>
+    <button @click="increment">Increment</button> -->
+
+  </div>
 </template>
+<style scoped>
+.main-view {
+  display: flex;
+}
+
+.restaurant-form {
+  border: 1px solid gray;
+  display: inline-block;
+  padding: 10px;
+  margin: 10px;
+}
+
+.restaurant-form input,
+.restaurant-form select {
+  width: 100%;
+}
+</style>
