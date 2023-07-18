e80b5333aa41ed8e84c3e72c78a4174bbad15063
Atom <dat101196@gmail.com>
2023-07-18 17:03:24 +0700

    Add function checkParent

--- demo-vue-flow/src/components/MainView.vue
+++ demo-vue-flow/src/components/MainView.vue
@@ -1,14 +1,13 @@
 <script setup lang="ts">
-import { VueFlow, useVueFlow, getRectOfNodes } from '@vue-flow/core'
+import { VueFlow, useVueFlow, getRectOfNodes, type GraphNode } from '@vue-flow/core'
 import { ref } from 'vue'
 import { initialElements } from '@/initial-elements'
-
 /**
  * You can either use `getIntersectingNodes` to check if a given node intersects with others
  * or `isNodeIntersecting` to check if a node is intersecting with a given area
  */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const { onNodeDrag, onNodeDragStop, getIntersectingNodes, isNodeIntersecting, getNodes } = useVueFlow()
+const { onNodeDrag, onNodeDragStop, getIntersectingNodes, isNodeIntersecting, getNodes, findNode } = useVueFlow()
 
 const elements = ref(initialElements)
 onNodeDragStop(({ event, node, nodes }) => {
@@ -25,29 +24,27 @@ onNodeDragStop(({ event, node, nodes }) => {
     //console.log('node: ', nodes[0])
     //console.log('all rect: ', getRectOfNodes(getNodes.value))
     getNodes.value.forEach((n) => {
+        console.log('--------------------')
+        console.log(n.label)
         console.log('n: ', n)
         if (n === node) {
             console.log('this node')
-        } 
-        else if (n.parentNode && n.parentNode == node.id) {
-            console.log('node children')
-        }
-        else {
-            console.log('--------------------')
-            console.log(n.label)
+        } else {
             const rect = getRectOfNodes([n])
             console.log('rect: ', rect)
-            const widthX = rect.x + rect.width
-            const heightY = rect.y + rect.height
-            console.log('widthX: ', widthX, '- heightY: ', heightY)
             let isIntersecting = isNodeIntersecting(node, rect)
             if (isIntersecting) {
+                if (checkParent(n, node.id)) {
+                    console.log('node children')
+                    return
+                }
+
                 if (node.parentNode !== n.id) {
                     const mergeRect = getRectOfNodes([node, n])
                     console.log('mergeRect: ', mergeRect)
                     console.log('isIntersecting: ', isIntersecting)
                     n.class = 'intersecting'
-                    
+
                     if (node.parentNode != n.id) {
                         n.position.x = mergeRect.x;
                         n.position.y = mergeRect.y;
@@ -71,6 +68,24 @@ onNodeDragStop(({ event, node, nodes }) => {
     })
 })
 
+function checkParent(nodeCheck: GraphNode, parentCheckID: string): boolean {
+    if (!nodeCheck.parentNode) {
+        return false;
+    }
+    console.log("[CheckParent] nodeCheck: ", nodeCheck.id)
+    console.log("[CheckParent] nodeCheck.parentNode: ", nodeCheck.parentNode)
+    console.log("[CheckParent] parentCheckID: ", parentCheckID)
+    if (nodeCheck.parentNode !== parentCheckID) {
+        const parent = findNode(nodeCheck.parentNode)
+        console.log("[CheckParent] parent node: ", parent)
+        if (parent) {
+            return checkParent(parent, parentCheckID);
+        }
+        return false;
+    }
+
+    return true;
+}
 // onNodeDrag(({ event, intersections, node, nodes }) => {
 //     //console.log('onNodeDrag')
 // })
